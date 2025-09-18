dcaa5851c2a9ae33bbcdbdba8005e28402bc9702
Ellexandrei Esponilla <xandrei37@gmail.com>
2025-09-18 19:11:04 +0800

    chore: setup tailwind and shadcn
    style: added lscs branding on landing page

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -1,17 +1,33 @@
 import React from 'react'
-import './styles.css'
+import { Inter as FontSans } from 'next/font/google'
+import '@/styles/globals.css'
+import { cn } from '@/lib/utils'
 
 export const metadata = {
-  description: 'A blank template using Payload in a Next.js app.',
-  title: 'Payload Blank Template',
+  description: 'A cms system for LSCS',
+  title: 'LSCS CMS',
+  icons: {
+    icon: '/lscs-logo.png',
+  },
 }
+
+const fontSans = FontSans({
+  subsets: ['latin'],
+  variable: '--font-sans',
+})
 
 export default async function RootLayout(props: { children: React.ReactNode }) {
   const { children } = props
 
   return (
-    <html lang="en">
-      <body>
+    <html lang="en" className="dark">
+      {/* Add 'dark' class for dark mode */}
+      <body
+        className={cn(
+          'min-h-screen bg-background text-foreground font-sans antialiased',
+          fontSans.variable,
+        )}
+      >
         <main>{children}</main>
       </body>
     </html>
--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -5,7 +5,7 @@ import React from 'react'
 import { fileURLToPath } from 'url'
 
 import config from '@/payload.config'
-import './styles.css'
+import { Button } from '@/components/ui/button'
 
 export default async function HomePage() {
   const headers = await getHeaders()
@@ -16,44 +16,56 @@ export default async function HomePage() {
   const fileURL = `vscode://file/${fileURLToPath(import.meta.url)}`
 
   return (
-    <div className="home">
-      <div className="content">
-        <picture>
-          <source srcSet="https://raw.githubusercontent.com/payloadcms/payload/main/packages/ui/src/assets/payload-favicon.svg" />
-          <Image
-            alt="Payload Logo"
-            height={65}
-            src="https://raw.githubusercontent.com/payloadcms/payload/main/packages/ui/src/assets/payload-favicon.svg"
-            width={65}
-          />
-        </picture>
-        {!user && <h1>Welcome to your new project.</h1>}
-        {user && <h1>Welcome back, {user.email}</h1>}
-        <div className="links">
-          <a
-            className="admin"
-            href={payloadConfig.routes.admin}
-            rel="noopener noreferrer"
-            target="_blank"
-          >
-            Go to admin panel
-          </a>
-          <a
-            className="docs"
-            href="https://payloadcms.com/docs"
-            rel="noopener noreferrer"
-            target="_blank"
-          >
-            Documentation
-          </a>
+    <div className="min-h-screen flex items-center justify-center bg-background text-foreground flex-col">
+      <div className="max-w-2xl mx-auto p-8 flex-1 flex justify-center flex-col">
+        <section className="flex items-center gap-4 mb-8 justify-center">
+          <picture>
+            <source srcSet="/lscs-logo.png" />
+            <Image alt="LSCS Logo" height={148} src="/lscs-logo.png" width={148} />
+          </picture>
+          <span>
+            <h1 className="sm:text-5xl text-2xl font-bold text-foreground">LSCS CMS</h1>
+            <h4 className="sm:text-sm text-xs font-extralight">40th La Salle Computer Society</h4>
+          </span>
+        </section>
+
+        <div className="mb-6">
+          {!user && (
+            <h2 className="text-lg text-muted-foreground text-center">
+              Welcome to your new project.
+            </h2>
+          )}
+          {user && (
+            <h2 className="text-lg text-center text-foreground">Welcome back, {user.email}</h2>
+          )}
+        </div>
+
+        <div className="flex gap-4 flex-col sm:flex-row justify-center">
+          <Button className="">
+            <a href={payloadConfig.routes.admin} rel="noopener noreferrer" target="_blank">
+              Go to admin panel
+            </a>
+          </Button>
+
+          <Button variant={'outline'}>
+            <a href="https://payloadcms.com/docs" rel="noopener noreferrer" target="_blank">
+              Documentation
+            </a>
+          </Button>
         </div>
       </div>
-      <div className="footer">
-        <p>Update this page by editing</p>
-        <a className="codeLink" href={fileURL}>
-          <code>app/(frontend)/page.tsx</code>
-        </a>
-      </div>
+      <footer className="text-sm font-extralight py-2 flex items-center">
+        <picture>
+          <source srcSet="https://avatars.githubusercontent.com/u/62968818?s=200&v=4" />
+          <Image
+            alt="LSCS Logo"
+            height={16}
+            src={'https://avatars.githubusercontent.com/u/62968818?s=200&v=4'}
+            width={16}
+          />
+        </picture>
+        Powered by Payload CMS
+      </footer>
     </div>
   )
 }
