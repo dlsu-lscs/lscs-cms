d157106267b62431fd9f302d2467eb92a06509bc
Ellexandrei Esponilla <xandrei37@gmail.com>
2025-09-21 18:51:45 +0800

    chore: created migration files

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,5 +1,6 @@
 import * as migration_20250919_051125 from './20250919_051125';
 import * as migration_20250919_052037 from './20250919_052037';
+import * as migration_20250921_105020 from './20250921_105020';
 
 export const migrations = [
   {
@@ -10,6 +11,11 @@ export const migrations = [
   {
     up: migration_20250919_052037.up,
     down: migration_20250919_052037.down,
-    name: '20250919_052037'
+    name: '20250919_052037',
+  },
+  {
+    up: migration_20250921_105020.up,
+    down: migration_20250921_105020.down,
+    name: '20250921_105020'
   },
 ];
