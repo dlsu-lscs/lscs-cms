0dd811bdfe0c997d1f81508cdef235dcb00b0bba
xandreiAThome <xandrei37@gmail.com>
2025-10-29 16:12:05 +0800

    chore: added migrations

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,6 +1,7 @@
 import * as migration_20250923_032716 from './20250923_032716';
 import * as migration_20251015_134112 from './20251015_134112';
 import * as migration_20251029_075900 from './20251029_075900';
+import * as migration_20251029_081041 from './20251029_081041';
 
 export const migrations = [
   {
@@ -16,6 +17,11 @@ export const migrations = [
   {
     up: migration_20251029_075900.up,
     down: migration_20251029_075900.down,
-    name: '20251029_075900'
+    name: '20251029_075900',
+  },
+  {
+    up: migration_20251029_081041.up,
+    down: migration_20251029_081041.down,
+    name: '20251029_081041'
   },
 ];
