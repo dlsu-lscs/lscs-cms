ff8cc9f5ec5acb19b4486564c591d08d7956708b
Ellexandrei Esponilla <xandrei37@gmail.com>
2026-01-25 10:38:11 +0800

    SGAR 2026 (#18)
    * feat: added units, exec board, and sgar media collections
    
    * fix: ci yml
    
    * fix: dockerfile and build script with migration
    
    * feat: implemented committee and positions collections
    
    chore: db migration
    
    * fix: lock package
    
    * fix: reset node modules
    
    * fix: fuck this shit Ill try pnpm at this point nothing to lose
    
    * fix: potanginang dockerignore
    
    * fix: db migration in dockerfile fuck
    
    * fix: migration again
    
    * revert dockerfile
    
    * fix: only admins can see all user acc
    
    * chore: added group for sgar collections
    
    * fix: added user perms for nextjs in container to upload media
    
    * fix: changed slug names
    
    * chore: created migration files
    
    * fix: joined dependent collections on unit collection
    
    * refactor: denormalized the collections
    
    * chore: created db migration files
    
    * fix: adjusted fields based on reqs
    
    chore: generated migration files
    
    * fix: added timeline field
    
    chore: migration files
    
    * fix: added org chart and cluster fields
    
    chore: migration files
    
    * chore: upate importmap
    
    * chore: added migration files
    
    * fix: fixed migration files

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -3,6 +3,7 @@ import * as migration_20251015_134112 from './20251015_134112';
 import * as migration_20251029_075900 from './20251029_075900';
 import * as migration_20251029_081041 from './20251029_081041';
 import * as migration_20251221_112400 from './20251221_112400';
+import * as migration_20260125_023627 from './20260125_023627';
 
 export const migrations = [
   {
@@ -28,6 +29,11 @@ export const migrations = [
   {
     up: migration_20251221_112400.up,
     down: migration_20251221_112400.down,
-    name: '20251221_112400'
+    name: '20251221_112400',
+  },
+  {
+    up: migration_20260125_023627.up,
+    down: migration_20260125_023627.down,
+    name: '20260125_023627'
   },
 ];
